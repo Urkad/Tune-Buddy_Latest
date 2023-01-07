4c8039f8b44a95f4df32bfd45bf5c8093bb51ee4
martinkascak <m.kascak93@gmail.com>
2023-01-07 19:54:13 +0100

    Create UserRepo

---
+++ backend/src/main/java/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package repo;
+
+
+import models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends MongoRepository<User,String> {
+
+}
