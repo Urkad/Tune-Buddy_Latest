a494a6637b299d6d93f6f4e3fa57af3293b79627
martinkascak <m.kascak93@gmail.com>
2023-01-07 20:55:29 +0100

    Add UserController

--- backend/src/main/java/controller/UserController.java
+++ backend/src/main/java/controller/UserController.java
@@ -1,15 +1,36 @@
 package controller;
 
 
+import models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+    private final UserService userService;
 
-
-    public UserController() {
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
+
 }
