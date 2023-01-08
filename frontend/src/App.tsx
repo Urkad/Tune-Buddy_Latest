9274fb2fb86cec30012f4c57f80bc796a65cba66
martinkascak <m.kascak93@gmail.com>
2023-01-08 11:36:17 +0100

    Add Home,Login,Profile pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import ProfilePage from "./components/ProfilePage";
+
 
 function App() {
+
+
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/api/home"} element={<HomePage></HomePage>}></Route>
+          <Route path={"/api/login"} element={<LoginPage />}></Route>
+          <Route path={"/api/profile"} element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
