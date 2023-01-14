4c48f33b85918ea46fedfc9982d850e716e52977
martinkascak <m.kascak93@gmail.com>
2023-01-14 20:01:37 +0100

    Add Navigation bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import {NavLink} from "react-router-dom";
+import {Button} from "@mui/material";
 
-export default function NavBar(){
-    return(
-        <div>Navbar</div>
+
+type NavigationBarProps = {
+    logout: () => Promise<string>
+}
+
+export default function NavigationBar(props: NavigationBarProps) {
+
+    return (
+        <div className={"nav-bar"}>
+            <NavLink to={"/"} >Startseite</NavLink>
+            <NavLink to={"/home"} >Home</NavLink>
+            <NavLink to={"/profile"} >Profile</NavLink>
+            <NavLink to={"/login"} >Login</NavLink>
+            <Button onClick={props.logout}>Logout</Button>
+        </div>
     )
 }
